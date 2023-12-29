644a80ab42dc97ab99c6c2eb2a4b119162597492
wuxintTLH <1392930618@qq.com>
2023-12-29 13:05:17 +0800

    first commit

---
+++ abBackgroundScript.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         AB站网页背景更改
+// @description  更改ab站背景的懒人脚本，引用vue和jquery优化
+// @icon         http://github.smiku.site/sakura.png
+// @license      MIT
+// @bilibili     https://space.bilibili.com/29058270
+// @github       https://github.com/wuxintlh/
+// @githubBoke   https://wuxintlh.github.io
+// @acfun        https://www.acfun.cn/u/57391284
+// @version      1.0.0.0
+// @author       SakuraMikku
+// @copyright    SakuraMikku
+// @match        *www.bilibili.com/*
+// @match        *://*.bilibili.com/*
+// @match        *message.bilibili.com/*
+// @match        *t.bilibili.com/*
+// @match        *manga.bilibili.com/*
+// @match        *live.bilibili.com/blackboard/*
+// @match        *www.bilibili.com/page-proxy/*
+// @match        *www.acfun.cn/*
+// @match        *.acfun.cn/*
+// @exclude      *live.bilibili.com/p/html/live-lottery/*
+// @exclude      *message.bilibili.com/pages/nav/index_new_pc_sync*
+// @exclude      *t.bilibili.com/pages/nav/index_new*
+// @exclude      *member.bilibili.com/x2/creative/*
+// @exclude      *member.bilibili.com/video/*
+// @exclude      *ink.bilibili.com/p/center/course/index.html*
+// @exclude      *www.bilibili.com/v/pay/charge*
+// @exclude      *message.acfun.cn/*
+// @exclude      *www.bilibili.com/bangumi*
+// @exclude      *account.bilibili.com/account/*
+// @exclude      *cm.bilibili.com/quests/*
+// @exclude      *member.bilibili.com/platform*
+// @exclude      *pay.bilibili.com/pay-v2-web*
+// @QQgroup      793513923
+// @QQgroup      https://jq.qq.com/?_wv=1027&k=0ewDiWw1
+// @grant        none
+// @require      https://greasyfork.org/scripts/445358-ab%E7%AB%99%E8%83%8C%E6%99%AF%E6%9B%B4%E6%94%B9css/code/ab%E7%AB%99%E8%83%8C%E6%99%AF%E6%9B%B4%E6%94%B9css.js?version=1053244
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js
+// @namespace https://greasyfork.org/users/599643
+// ==/UserScript==
+
+
+"use strict"
+
+window.onload = () => {
+
+}
