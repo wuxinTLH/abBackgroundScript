d6fa59cb54f243bdaa629d5af25490df55afe0ee
wuxintlh <1392930618@qq.com>
2024-04-14 21:13:28 +0800

    版本重置,正在重构整个代码,取消了vue以避免与b站原有vue冲突 使用ES6+等特性对整个脚本进行性能上等的提升

--- abBackgroundScript.js
+++ abBackgroundScript.js
@@ -15,10 +15,7 @@
 // @github       https://github.com/wuxintlh/
 // @githubBoke   https://wuxintlh.github.io
 // @acfun        https://www.acfun.cn/u/57391284
-// @require      https://greasyfork.org/scripts/445358-ab%E7%AB%99%E8%83%8C%E6%99%AF%E6%9B%B4%E6%94%B9css/code/ab%E7%AB%99%E8%83%8C%E6%99%AF%E6%9B%B4%E6%94%B9css.js?version=1053244
-// @require      https://update.greasyfork.org/scripts/483725/1305374/%E6%B5%8F%E8%A7%88%E5%99%A8%E6%95%B0%E6%8D%AE%E5%BA%93API.js
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js
 // @match        *www.bilibili.com/*
 // @match        *://*.bilibili.com/*
 // @match        *message.bilibili.com/*
@@ -44,295 +41,62 @@
 // ==/UserScript==
 
 "use strict";
+
 window.onload = () => {
+    firstTimeInfoAlert();
     init();
-
-    //#region 封装方法
-    function init() {
-        //获取host,选择分支
-        let abChosen, liveFlag, host = window.location.host;
-
-        //判断ab站
-        if (host.indexOf("bilibili") != -1) { //b站
-            abChosen = "bili";
-            //是否为直播
-            if (host == "live.bilibili.com") { //是
-                liveFlag = true;
-            } else { //不是
-                liveFlag = false;
-            }
-        } else if (host.indexOf("acfun") != -1) { //a站
-            abChosen = "acfun";
-            liveFlag = false
-        }
-        //default_background_urls考虑采用http请求一个接口来获取默认图片地址
-        let default_background_urls = {
-            'bili': [
-                'https://i0.hdslb.com/bfs/article/d12fee446e2533206e0b04024c39e00a40c4bc4c.png@1320w_912h.webp',
-                'https://i0.hdslb.com/bfs/article/54616fdbb9bed40ea2cf8540f8517b11c9aa4ad3.jpg@1320w_868h.webp',
-                'https://img1.imgtp.com/2022/05/19/qqKLSTQo.png',
-                'https://i0.hdslb.com/bfs/album/658ab52e2d631f9d974112e2d5b4cab476e3f61d.jpg',
-                'https://i0.hdslb.com/bfs/vc/c255f51c594cf6e724fb9f04975fae7e7eb8b876.jpg@2000w_1e.webp',
-                'https://w.wallhaven.cc/full/o3/wallhaven-o31p97.jpg'
-            ],
-            'acfun': [
-                'https://w.wallhaven.cc/full/g8/wallhaven-g8kd37.jpg',
-                'https://img.tt98.com/d/file/96kaifa/201905101622281/001.jpg',
-                'https://img.tt98.com/d/file/tt98/2019092618001803/001.jpg',
-                'https://w.wallhaven.cc/full/g7/wallhaven-g79ov3.jpg',
-                'https://w.wallhaven.cc/full/rd/wallhaven-rdyyjm.png',
-                'https://w.wallhaven.cc/full/o3/wallhaven-o31p97.jpg'
-            ]
-        }
-        //初始化bckgroundBox
-        setBackgroundBox(abChosen);
-        //将backgroundBox进行vue绑定
-        let BackgroundBoxVue = new Vue({
-            el: '#sakuraBackgroundBox',
-            data: {
-                default_url: default_background_urls[abChosen],
-            },
-            methods: {
-                displayChangeBox: function () {
-                    let changeBox = $('.ChangeBox');
-                    changeBox.css('display', changeBox.css('display') === 'none' ? 'block' : 'none');
-                },
-                defaultBackgroundChange: function (src) {
-
-                },
-                diyBackgroundChange: function () {
-                    if ($("#diyInput").val() == "" || $("#diyInput").val() == null) {
-                        alert("请不要输入空值");
-                        return null;
-                    } else {
-                        //设置背景
-                        let url = $("#diyInput").val();
-                    }
-                },
-                diyBase64Change: function () {
-                    let file = $("#base64Pic")[0].files[0];
-                    let reader = new FileReader();
-                    if (file) {
-                        reader.readAsDataURL(file);
-                        reader.onloadend = function () {
-                            // console.log(reader.result);
-                            setBackgroundImage(reader.result, ab_chosen);
-                        }
-                    } else {
-                        alert("请上传一张图片");
-                    }
-                },
-                deleteURL: function () {
-                    if (confirm("确定要删除背景吗？")) {
-                        //调用indexedDBAPI,删除URL或图片base64格式
-                    }
-                }
-            }
-        });
-        //生成背景div
-        setDivClassSakuraBackground(abChosen);
-        //生成背景
-        let bcurl = window.localStorage.getItem('bcurl');
-        bcurl = bcurl && bcurl.trim() !== '' ? bcurl : '默认bcurl';
-        setBackgroundImage(url, abChosen);
-    }
-
-    /**
-     * @description 对ab站进行Box的创建
-     * @param {String} abChosen 
-     */
-    function setBackgroundBox(abChosen) {
-        let elBody = getElBody(abChosen);
-        let father_node = $(elBody)[0];
-        let background_box = document.createElement("div");
-        father_node.appendChild(background_box);
-        background_box.innerHTML = `
-    <button class="clickButton" v-on:click="displayChangeBox()">更改背景</button>
-    <div class="ChangeBox">
-        <h4>更改背景</h4>
-        <div class="defaultBox">
-            <img :src="default_url[0]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[0])">
-            <img :src="default_url[1]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[1])">
-            <img :src="default_url[2]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[2])">
-            <img :src="default_url[3]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[3])">
-            <img :src="default_url[4]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[4])">
-            <img :src="default_url[5]" alt="" class="defaultImage" v-on:click="defaultBackgroundChange(default_url[5])">
-        </div>
-        <div class="diyBox">
-            <input type="text" name="" id="diyInput" placeholder="请输入背景URL">
-            <button id="diySubmit" v-on:click="diyBackgroundChange()">点击修改</button>
-            <input type="file" name="" id="base64Pic">
-            <button id="diySubmit" v-on:click="diyBase64Change()">点击修改</button>
-            <button id="diySubmit" v-on:click="deleteURL()">删除URL</button>
-        </div>
-    </div>
-    `;
-        background_box.id = "sakuraBackgroundBox";
-        //console.log("sakuraBackgroundBox创建完毕");
-
-        //根据elBody进行BackgroundBox的生成
-
-        /*
-        if (abChosen == "bili") {
-            if (liveFlag) {
-                //直播
-            } else {
-                //非直播
-            }
-        } else {
-        }
-        */
-    }
-
-
-    /**
-     * @description 根据ab站获取对应的vue绑定Element
-     * @param {String} abChosen 
-     * @returns {String} elBody
-     */
-    function getElBody(abChosen) {
-        let elBody;
-        if (abChosen == "bili") {
-            if ($("#app").length > 0) {
-                elBody = "#app";
-            } else {
-                elBody = "body";
-            }
-        } else if (abChosen == "acfun") {
-            if ($("#main").length > 0) {
-                elBody = "#main";
-            } else {
-                elBody = "body";
-            }
-        }
-        return elBody;
-    }
-
-
-    //主程序 设置背景
-    /**
-     * 
-     * @param {String} url - 图片url或base64
-     * @param {*} abChosen 
-     */
-    function setBackgroundImage(url, abChosen) {
-        let sakuraBackgroundElement, body = $("body")[0];
-
-        // 尝试保存地址，如果没有保存成功，则不会进行接下来的代码
-        let api = IndexedDBAPI();
-        let saveResult = api.addBackgroundURL(url);
-        if (!saveResult) {
-            logSakuraBackgroundInfo("保存失败,在写入数据库时发生错误,没有任何背景受到更改");
-            return;
-        }
-
-        if (abChosen == "bili") {
-            if ($("#app").length > 0) {
-                sakuraBackgroundElement = "#app";
-            } else {
-                if ($(".sakuraBackground").length > 0) {
-                    // 存在就直接使用
-                    sakuraBackgroundElement = ".sakuraBackground";
-                } else {
-                    // 不存在就创建一个
-                    let div = document.createElement("div")
-                    body.appendChild(div);
-                    div.className = "sakuraBackground";
-                    sakuraBackgroundElement = "sakuraBackground";
-                }
-            }
-            $(sakuraBackgroundElement).css({
-                'background': 'url(' + url + ')',
-                'background-repeat': 'no-repeat',
-                'position': 'fixed',
-                'background-position': 'center center',
-                'background-size': 'cover',
-                'zoom': '1',
-                'width': '100%',
-                'height': '100%',
-                'top': '0',
-                'left': '0',
-                'webkit-background-size': 'cover',
-                'z-index': '-1',
-                'class': 'SakuraBackground'
-            });
-        } else if (abChosen == "acfun") {
-            if ($('#main').length > 0) {
-                sakuraBackgroundElement = "#main";
-            } else {
-                if ($(".sakuraBackground").length > 0) {
-                    // 存在就直接使用
-                    sakuraBackgroundElement = ".sakuraBackground";
-                } else {
-                    // 不存在就创建一个
-                    let div = document.createElement("div")
-                    body.appendChild(div);
-                    div.className = "sakuraBackground";
-                    sakuraBackgroundElement = "sakuraBackground";
-                }
-            }
-            $(sakuraBackgroundElement).css({
-                'background-image': 'url(' + url + ')',
-                'background-repeat': 'no-repeat',
-                'position': 'fixed',
-                'background-position': 'center center',
-                'background-size': 'cover',
-                'zoom': '1',
-                'width': '100%',
-                'height': '100%',
-                'top': '0',
-                'left': '0',
-                'webkit-background-size': 'cover',
-                'z-index': '-1',
-                'class': 'SakuraBackground'
-            });
-        }
-        // setDivClassSakuraBackground(url, elBody);
-    }
-
-    //背景div生成
-    function setDivClassSakuraBackground(url, elBody) {
-        if ($('#sakuraBackground')) {
-
-        }
-    }
-
-
-    /**
-     * @returns {String} 返回一个url或base64值
-     */
-    function getBcurl() {
-        let url = window.localStorage.getItem('bcurl');
-        return url;
-    }
-    //indexedDBAPI转移至js中,使用require调用相应js
-
-    /**
-     * @name 首次信息提示
-     * @description 首次alert会提示脚本注意事项,并且在每次脚本启动时,都会在控制台中打印提示信息
-     * @returns {void}
-     */
-    function checkSakuraBackgroundInfoAlert() {
-        // 检查 localStorage 中的键值对
-        let alertShown = localStorage.getItem("SakuraBackgroundInfoAlert");
-        let info = `特别提示:本脚本受图片质量影响,如果使用过大图片,容易使indexedDB读取缓慢,
-        浏览器性能极大下降,请谨慎使用本地图片.网络图片不会影响,但受网络速率,是否有墙等因素影响.
-        脚本作者:SakuraMikku.有问题请在脚本界面查询联系或反馈方式`;
-        if (!alertShown) {
-            alert(info);
-            localStorage.setItem("SakuraBackgroundInfoAlert", "true");
-        }
-        console.log(`[SakuraBackgroundInfo]  ${info}`)
-    }
-    /**
-     * @name 封装的console.log
-     * @description 用于输出脚本信息
-     * @param  {...any} args 
-     */
-    function logSakuraBackgroundInfo(...args) {
-        let prefixedArgs = args.map(arg => "[SakuraBackgroundInfo] " + arg);
-        console.log.apply(this, prefixedArgs);
-    }
-
-    //#endregion
 }
+//#region 封装方法
+/**
+ * @name init
+ * @description 脚本一些内容的初始化
+ *
+ */
+function init() {
+    //初始化脚本更改盒子
+
+
+    //初始化盒子中的脚本
+
+
+    //初始化背景
+
+}
+
+
+
+/**
+ * @name firstTimeInfoAlert
+ * @description 第一次进入提示
+ */
+function firstTimeInfoAlert() {
+    let firstTime = localStorage.getItem("SakuraBackgroundScriptFirstTimeInfo");
+    if (!firstTime) {
+        //未提示过,则提示使用
+        alert(`欢迎使用SakuraBackground脚本\n这是第一次使用脚本的提示\n以后不会再提醒`);
+        localStorage.setItem("SakuraBackgroundScriptFirstTimeInfo", true);
+    }
+    //已经提示过,则不会再提示
+}
+
+/**
+ * @name logSakuraBackgroundInfo
+ * @description 输出日志
+ * @param {*} type 
+ * @param  {...any} obj 
+ * @return noreturn
+ */
+function logSakuraBackgroundInfo(type, ...objs) {
+    //设置日志信息
+    let date = new Date();
+    let strs = `[SakuraBackgroundScript] ${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    //将信息并入strs
+    objs.forEach(element => {
+        strs += ` ${element}`;
+    });
+    //输出日志
+    console[type](strs);
+}
+
+
+//#endregion
