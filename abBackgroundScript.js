d91dac6d3d2486c32895612b35a88a796c0e3f3f
wuxinTLH <1392930618@qq.com>
2024-04-15 10:25:12 +0800

    增加了storageAPI方法

--- abBackgroundScript.js
+++ abBackgroundScript.js
@@ -42,6 +42,11 @@
 
 "use strict";
 
+//#region 全局数据存储
+var backgroundURLS = [];
+
+//#endregion
+
 window.onload = () => {
     firstTimeInfoAlert();
     init();
@@ -53,8 +58,27 @@ window.onload = () => {
  *
  */
 function init() {
+    //对ab站检测
+    let isAB = window.location.host.includes("bilibili.com") ? 'bili' : 'acfun';
     //初始化脚本更改盒子
-
+    let backgroundMainElement;
+    //获取ab站的初始化节点
+    if (isAB == 'bili') {
+        if ($('#app')) {
+            backgroundMainElement = $('#app');
+        } else if ($('#main')) {
+            backgroundMainElement = $('#main');
+        } else {
+            backgroundMainElement = $('body');
+        }
+    } else if (isAB == 'acfun') {
+        backgroundMainElement = $('body');
+    }
+    //对盒子进行初始化
+    let sakuraBackgroundBox = document.createElement('div');
+    let showSakuraBackgroundBox = document.createElement('div');
+    //初始化盒子的样式
+    let sakuraBackgroundCssStyles = ``;
 
     //初始化盒子中的脚本
 
@@ -79,11 +103,203 @@ function firstTimeInfoAlert() {
     //已经提示过,则不会再提示
 }
 
+
+
+/**
+ * @name storageAPI
+ * @description 封装存储API
+ * @returns Function
+ * @example
+ * let storage = new storageAPI();
+ * storage.xxx();
+ * 
+ */
+function storageAPI() {
+    //初始化变量信息
+    //dbName为数据库名称,storeName为存储名称
+    const dbName = 'backgroundURL';
+    const storeName = 'SakuraBCUrl';
+    let operationResult;
+    //初始化indexedDB,用于测试浏览器是否支持indexedDB
+    let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    if (!indexedDB) {
+        //不支持indexedDB使用localStorage
+        /**
+         * @name addBackgroundURL
+         * @description 用于存储背景url或base64
+         * @param {string} url 
+         * @returns JSON数据
+         */
+        const addBackgroundURL = function (url) {
+            try {
+                //获取url长度
+                let urlLength = url.length;
+                //如果长度大于剩余空间则直接返回空间不足
+                let remainingSpace = 5 * 1024 * 1024 - new Blob([JSON.stringify(localStorage)]).size
+                if (url.length > remainingSpace) {
+                    throw new Error('localStorage剩余容量不够使其保存图片base64格式或图片地址');
+                }
+                //如果可以存下,则直接存储
+                if (localStorage.setItem(storeName, url)) {
+                    return {
+                        "status": true,
+                        "message": "保存成功"
+                    };
+                } else {
+                    throw new Error('存储时存在错误,原因可能为剩余容量不够');
+                }
+
+            } catch (error) {
+                logSakuraBackgroundInfo("error", error);
+                return {
+                    "status": false,
+                    "message": "存储失败"
+                };
+            }
+        }
+        /**
+         * @name getBackgroundURL
+         * @description 获取背景url或base64 
+         * @returns 背景url或base64
+         */
+        const getBackgroundURL = function () {
+            //获取url
+            let url = localStorage.getItem(storeName);
+            return url;
+        }
+        /**
+         * @name deleteBackgroundURL
+         * @description 删除背景url或base64 
+         * @returns JSON数据
+         */
+        const deleteBackgroundURL = function () {
+            //删除url
+            if (localStorage.removeItem(storeName)) {
+                return {
+                    "status": true,
+                    "message": "删除成功"
+                }
+            } else {
+                return {
+                    "status": false,
+                    "message": "删除失败"
+                }
+            }
+        }
+        return {
+            addBackgroundURL: addBackgroundURL,
+            getBackgroundURL: getBackgroundURL,
+            deleteBackgroundURL: deleteBackgroundURL
+        }
+
+    } else {
+        //支持则使用indexedDB
+        let db;
+        let request = indexedDB.open(dbName, 1);
+        //监听事件
+        //打开时报错
+        request.onerror = function (event) {
+            logSakuraBackgroundInfo("error", "indexedDB打开失败");
+        };
+        //打开时阻塞
+        request.onblocked = function (event) {
+            logSakuraBackgroundInfo("error", "indexedDB打开被阻止");
+        };
+        //打开时升级
+        request.onupgradeneeded = function (event) {
+            db = event.target.result;
+            if (!db.objectStoreNames.contains(storeName)) {
+                let objectStore = db.createObjectStore(storeName, { keyPath: "id" });
+            }
+        };
+        //成功打开
+        request.onsuccess = function (event) {
+            db = event.target.result;
+        };
+
+        const addBackgroundURL = function (url) {
+            //设置一个5MB大小的分片
+            let chunkSize = 5 * 1024 * 1024;
+            let chunks = [];
+            //对chunks赋值
+            for (let i = 0; i < url.length; i += chunkSize) {
+                chunks.push(url.slice(i, i + chunkSize));
+            }
+            //调用deleteBackgroundURL删除所有分片
+            this.deleteBackgroundURL();
+
+            //创建事务
+            let transaction = db.transaction([storeName], "readwrite");
+            //获取objectStore
+            let objectStore = transaction.objectStore(storeName);
+            //遍历chunks,存入indexedDB中
+            chunks.forEach(function (chunk, index) {
+                objectStore.add({ id: index, data: chunk });
+            });
+            return {
+                "status": true,
+                "message": "保存成功"
+            };
+
+        }
+
+        const getBackgroundURL = function () {
+            //获取所有分片
+            let transaction = db.transaction([storeName], "readonly");
+            //获取objectStore
+            let objectStore = transaction.objectStore(storeName);
+            //获取所有分片
+            let request = objectStore.getAll();
+            //监听事件
+            request.onsuccess = function (event) {
+                //获取结果
+                let result = event.target.result;
+                let url = "";
+                //遍历result,拼接url
+                result.forEach(function (chunk) {
+                    url += chunk.data;
+                });
+                //返回url
+                return url;
+            };
+            //监听事件
+            request.onerror = function (event) {
+                logSakuraBackgroundInfo("error", "获取背景url或base64失败");
+            };
+        }
+
+        const deleteBackgroundURL = function () {
+            //创建事务
+            let transaction = db.transaction([storeName], "readwrite");
+            //获取objectStore
+            let objectStore = transaction.objectStore(storeName);
+            //删除所有分片
+            objectStore.clear();
+            //返回结果
+            return {
+                "status": true,
+                "message": "删除成功"
+            };
+        }
+        const closeIndexedDB = function () {
+            if (db) {
+                db.close();
+            }
+        }
+        return {
+            addBackgroundURL: addBackgroundURL,
+            getBackgroundURL: getBackgroundURL,
+            deleteBackgroundURL: deleteBackgroundURL,
+            closeIndexedDB: closeIndexedDB
+        }
+    }
+}
+
 /**
  * @name logSakuraBackgroundInfo
  * @description 输出日志
- * @param {*} type 
- * @param  {...any} obj 
+ * @param {string} type 
+ * @param  {...string} obj 
  * @return noreturn
  */
 function logSakuraBackgroundInfo(type, ...objs) {
@@ -97,6 +313,4 @@ function logSakuraBackgroundInfo(type, ...objs) {
     //输出日志
     console[type](strs);
 }
-
-
 //#endregion
